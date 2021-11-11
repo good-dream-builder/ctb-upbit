23a8ca2b939b5c07b8043a3f42ac252d96b6d515
good.dream.builder <good.dream.builder@gmail.com>
2021-11-12 00:55:44 +0900

    [일반] 서버 기동 시간 확인 로그

--- src/main/java/com/zzup/ctbupbit/CtbUpbitApplication.java
+++ src/main/java/com/zzup/ctbupbit/CtbUpbitApplication.java
@@ -4,13 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
-public class
-CtbUpbitApplication {
+public class CtbUpbitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CtbUpbitApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CtbUpbitApplication.class, args);
+    }
+
+    @PostConstruct
+    public void afterStarted() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("서버 현재 시각 : " + new Date());
+    }
 
 }
